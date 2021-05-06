f39b78bb479742fc98a03c1ed67315a3331b7fd2
Roman Matrosov <crazy98@bk.ru>
2021-05-06 23:48:23 +0300

    Lesson 24 v.2

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
 'use strict';
-'use strict';
-
-let numberOfFilms = '';
 
 let personalMovieDB = {
-	count: numberOfFilms,
+	count: '',
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false,
+	start: function() {
+		while(this.count == '' || this.count == null || isNaN(this.count)) {
+			this.count = +prompt('Сколько фильмов вы уже посмотрели?');
+		}
+		// this.rememberMyFilms();
+		// this.detectPersonalLevel();
+		this.writeYourGenres();
+		this.showMyDB();
+	},
 	rememberMyFilms: function() {
 		for(let i = 0; i < 2; i++) {
 			const filmData = this.getFilm();
@@ -79,14 +72,4 @@ let personalMovieDB = {
 	}
 }
 
-start();
-
-function start() {
-	while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-	}
-	// personalMovieDB.rememberMyFilms();
-	// personalMovieDB.detectPersonalLevel();
-	personalMovieDB.writeYourGenres();
-	personalMovieDB.showMyDB();
-}
+personalMovieDB.start();
